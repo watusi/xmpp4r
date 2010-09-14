633fe825446c7576e336d5ba304ea084f13ac47e
Huw Giddens <huw@robotines.co.nz>
2010-09-14 20:07:48 +0800

    Don't reduce pending_requests below 0 when unneeded requests are added.

--- lib/xmpp4r/httpbinding/client.rb
+++ lib/xmpp4r/httpbinding/client.rb
@@ -188,7 +188,8 @@ module Jabber
           begin
             @lock.synchronize {
               # Do not send unneeded requests
-              if data.size < 1 and @pending_requests > 0
+              @pending_requests += 1
+              if data.size < 1 and @pending_requests > 1
                 return
               end
 
@@ -201,7 +202,6 @@ module Jabber
               req_body += "</body>"
               current_rid = @http_rid
 
-              @pending_requests += 1
               @last_send = Time.now
             }
 
